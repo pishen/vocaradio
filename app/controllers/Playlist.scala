c7f36c7095b50c199fadc2747707a01b8b04af16
pishen <pishen02@gmail.com>
2013-10-10 00:00:49 +0800

    re-pick if youtube api parsing exception
    Former-commit-id: 2b0f01f5661dd4e44c1dd4b56a81bd35bb889226 [formerly b42d1564fc7b00631c394fbf82d348fbd647193b] [formerly 4bcd3157581cebb0ba61ee6eb7df19027645da08 [formerly 10ce476e445da523fd2b808bfab30d39e645e834]]
    Former-commit-id: d1f8a2927c7f378748e9fd756b2c340f22fecedc [formerly 804bbb18883dc7bf5d9ed3807d35aacc1e925b12]
    Former-commit-id: da93d921eefd9fa72c7b100c82a3fb2362fe42a6

--- app/controllers/Playlist.scala
+++ app/controllers/Playlist.scala
@@ -15,16 +15,16 @@ import java.util.Date
 class Playlist extends Actor {
   private val titles = Source.fromFile("titles").getLines.toIndexedSeq
   private val googleKey = Source.fromFile("google-api-key").getLines.toSeq.head
-  
+
   private var playing: Song = randomPick()
   private var startTime: Long = getCurrentTimeInSecond()
 
   def receive = {
     case Playlist.Current => {
       val timePassed = getCurrentTimeInSecond() - startTime
-      if(timePassed < playing.duration){
+      if (timePassed < playing.duration) {
         sender ! (playing.id, timePassed.toInt)
-      }else{
+      } else {
         playing = randomPick()
         startTime = getCurrentTimeInSecond()
         sender ! (playing.id, 0)
@@ -32,31 +32,35 @@ class Playlist extends Actor {
     }
   }
 
-  private def randomPick() = {
+  private def randomPick(): Song = {
     val title = helper.urlEncode(titles(Random.nextInt(titles.length)))
-    val futureId =
-      WS.url("https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q="
-        + title
-        + "&type=video&fields=items%2Fid&key="
-        + googleKey)
-        .get
-        .map(response => (response.json \\ "videoId").head.as[String])
-    val id = Await.result(futureId, 5.seconds).asInstanceOf[String]
-    val futureDuration =
-      WS.url("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id="
-        + id
-        + "&fields=items%2FcontentDetails&key="
-        + googleKey)
-        .get
-        .map(response => (response.json \\ "duration").head.as[String])
-        .map(str => {
-          val mAndS = str.replaceAll("[PTS]", "").split("M")
-          mAndS.head.toInt * 60 + mAndS.last.toInt
-        })
-    val duration = Await.result(futureDuration, 5.seconds).asInstanceOf[Int]
-    Song(id, duration)
+    try {
+      val futureId =
+        WS.url("https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q="
+          + title
+          + "&type=video&fields=items%2Fid&key="
+          + googleKey)
+          .get
+          .map(response => (response.json \\ "videoId").head.as[String])
+      val id = Await.result(futureId, 5.seconds).asInstanceOf[String]
+      val futureDuration =
+        WS.url("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id="
+          + id
+          + "&fields=items%2FcontentDetails&key="
+          + googleKey)
+          .get
+          .map(response => (response.json \\ "duration").head.as[String])
+          .map(str => {
+            val mAndS = str.replaceAll("[PTS]", "").split("M")
+            mAndS.head.toInt * 60 + mAndS.last.toInt
+          })
+      val duration = Await.result(futureDuration, 5.seconds).asInstanceOf[Int]
+      Song(id, duration)
+    } catch {
+      case ex: Exception => randomPick()
+    }
   }
-  
+
   private def getCurrentTimeInSecond() = new Date().getTime() / 1000
 }
 
