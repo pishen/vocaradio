a9332b8e726de8a37a1b9f827fd20f7e5f108d81
pishen <pishen02@gmail.com>
2013-12-11 02:19:14 +0800

    new update logic, with some update and delete ad-hoc code
    Former-commit-id: dd0c26c6fbcbdce092c7f89de6233ec50a9b6b6e [formerly b89ae600234dc69fa916755080f76bd6193d7963] [formerly 788f6396d570f3c01709521b33884fdc9f56831f [formerly 1f0c1e652d55c4297e2ac7f61ffdc2c5f4477eb6]]
    Former-commit-id: cfc1fc724441898ec92fce782c886748da023ee4 [formerly eab6d654bad2bbe91c60d731a49c2495a6b7d9c7]
    Former-commit-id: 6f775076db1031140dcd2c5ef09153c1ab29d314

--- app/controllers/MusicStore.scala
+++ app/controllers/MusicStore.scala
@@ -1,20 +1,20 @@
 package controllers
 
 import scala.concurrent.Future
+
 import org.anormcypher.Cypher
 import org.anormcypher.CypherRow
+
 import models.Song
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.ws.WS
 import scalax.io.Resource
 import views.html.helper
-import play.api.libs.concurrent.Akka
-import akka.actor.Actor
 
 object MusicStore {
   private val googleKey = Resource.fromFile("google-api-key").lines().head
 
-  def updateSong(originTitle: String): Future[Song] = {
+  def updateAndGetSong(originTitle: String): Future[Song] = {
     val inDB = Cypher("MATCH (s:Song) WHERE s.originTitle = {originTitle} RETURN s.videoId")
       .on("originTitle" -> originTitle).apply.collect {
         case CypherRow(vid: String) => vid
@@ -23,48 +23,40 @@ object MusicStore {
     if (inDB.nonEmpty) {
       //update song in DB by the saved id if it's not error
       val videoId = inDB.head
-      val songF = getSong(originTitle, if (videoId != "error") videoId else null)
-      songF.foreach(song => {
-        val res = Cypher("""
-          MATCH (s:Song)
-          WHERE s.originTitle = {originTitle}
-          SET s.videoId = {videoId}, s.title = {title}, s.duration = {duration}
-          """).on("originTitle" -> originTitle,
-          "videoId" -> song.videoId,
-          "title" -> song.title,
-          "duration" -> song.duration).execute
-        //TODO log the error for Cypher
-      })
-      songF
+      getSong(originTitle, videoId).recoverWith {
+        case ex: Exception => {
+          val recovered = getSong(originTitle).recover {
+            case ex: Exception => Song.error(originTitle)
+          }
+          recovered.foreach(song => {
+            val res = Cypher("MATCH (s:Song) WHERE s.originTitle = {originTitle} SET s.videoId = {videoId}")
+              .on("originTitle" -> originTitle, "videoId" -> song.videoId).execute
+            //TODO log the error for Cypher
+          })
+          recovered
+        }
+      }
     } else {
       //add new song
-      val songF = getSong(originTitle)
-      songF.foreach(song => {
-        val res = Cypher("""
-          CREATE (s:Song {
-            originTitle : {originTitle},
-            videoId : {videoId},
-            title : {title},
-            duration : {duration}
-          })
-          """).on("originTitle" -> originTitle,
-          "videoId" -> song.videoId,
-          "title" -> song.title,
-          "duration" -> song.duration).execute
+      val newSong = getSong(originTitle).recover {
+        case ex: Exception => Song.error(originTitle)
+      }
+      newSong.foreach(song => {
+        val res = Cypher("CREATE (s:Song {originTitle:{originTitle}, videoId:{videoId}})")
+          .on("originTitle" -> originTitle, "videoId" -> song.videoId).execute
         //TODO log the error for Cypher
       })
-      songF
+      newSong
     }
   }
 
   private def getSong(originTitle: String, videoId: String = null): Future[Song] = {
-    val f = for {
+    for {
       videoId <- if (videoId == null) getVideoId(originTitle) else Future.successful(videoId)
       (title, duration) <- getDetails(videoId)
     } yield {
       Song(originTitle, videoId, title, duration)
     }
-    f.recover { case e: Exception => Song.error(originTitle) }
   }
 
   private def getVideoId(originTitle: String): Future[String] = {
@@ -96,9 +88,29 @@ object BatchUpdater {
 
   def main(args: Array[String]): Unit = {
     val titles = Resource.fromFile("titles").lines().toSeq
-    titles.foreach(title => {
-      MusicStore.updateSong(title).foreach(song => println(title + ": " + song.videoId))
-    })
+    //delete 'not in titles' songs
+    /*val inDB = Cypher("MATCH (s:Song) RETURN s.originTitle").apply.collect {
+      case CypherRow(originTitle: String) => originTitle
+    }
+    inDB.filterNot(titles contains _).foreach(originTitle => {
+      val res = Cypher("MATCH (s:Song) WHERE s.originTitle = {originTitle} DELETE s")
+        .on("originTitle" -> originTitle).execute
+      println("delete " + originTitle + " with status " + res)
+    })*/
+    //update by titles
+    /*titles.foreach(title => {
+      MusicStore.updateAndGetSong(title).foreach(song => println(title + " updated as " + song.videoId))
+    })*/
+    //update by replaces
+    Resource.fromFile("replaces").lines().toSeq
+      .map(_.split(">>>")).foreach(s => {
+        val originTitle = s.head
+        val newId = s.last
+        val res = Cypher("MATCH (s:Song) WHERE s.originTitle = {originTitle} SET s.videoId = {videoId}")
+          .on("originTitle" -> originTitle, "videoId" -> newId).execute
+        println("replace " + originTitle + " with status " + res)
+      })
+
     println("main done")
   }
 
--- app/models/Song.scala
+++ app/models/Song.scala
@@ -3,5 +3,6 @@ package models
 case class Song(originTitle: String, videoId: String, title: String, duration: Int)
 
 object Song {
-  def error(originTitle: String) = Song(originTitle, "error", "error", 0)
+  //TODO think a better way to do this
+  def error(originTitle: String) = Song(originTitle, "error", "", 0)
 }
