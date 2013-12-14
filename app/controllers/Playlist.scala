367efafe1b6a5acc3d8e07f01ac87889666e0148
pishen <pishen02@gmail.com>
2013-12-14 17:49:02 +0800

    connect Playlist with MusicStore
    Former-commit-id: a84e1c2dd86fb8685a8738f987ec514f2b0c92d9 [formerly b4ff8e9ddf88cc7ade212f2ebada1da52fb46fcc] [formerly 7d127fdbb8d9e3f4a49cd7dd35eab52d7b151246 [formerly f36cb0150c68a211c7de4fe91dd4eae943e9a865]]
    Former-commit-id: b815237a4509171d0635bbf827a49b3013038284 [formerly 9f92b1db2c99b01967af48589c1965a8bc0c8ba9]
    Former-commit-id: 06f344fd822cfb57d04cfbcd3e5c8ad1f625e8f6

--- app/controllers/Playlist.scala
+++ app/controllers/Playlist.scala
@@ -17,90 +17,49 @@ import play.api.libs.json.Json
 
 class Playlist extends Actor {
   private val titlesResource = Resource.fromFile("titles")
-  private val replacesResource = Resource.fromFile("replaces")
   private val googleKey = Resource.fromFile("google-api-key").lines().head
   private val broadcaster = context.actorSelection("../broadcaster")
   private val chatLogger = context.actorSelection("../chatLogger")
 
-  private def currentSecond() = new Date().getTime() / 1000
-  private var titleSeq = Random.shuffle(titlesResource.lines().toSeq)
-  private var futureSong = pickWithTime()
+  private def getTime() = new Date().getTime() / 1000
+  private var titleSeq = Random.shuffle(titlesResource.lines().toSeq).take(21)
+  private var nextSong = getNextAndFill()
 
   def receive = {
     case AskSong => {
-      futureSong.value match {
-        case Some(Success(t)) =>
-          if (currentSecond - t._2 >= t._1.duration - 2) {
-            futureSong = pickWithTime()
+      nextSong.value match {
+        case Some(Success((song, start))) =>
+          if (getTime - start > song.duration - 2) {
+            nextSong = getNextAndFill()
           }
-        case Some(Failure(e)) =>
-          futureSong = pickWithTime()
-        case _ => //do nothing
+        case Some(Failure(e)) => nextSong = getNextAndFill()
+        case None             => //song not retrieved yet
       }
-      futureSong.map {
-        case (song, startTime) =>
-          (song.videoId, (currentSecond - startTime).toInt, song.originTitle)
+      nextSong.map {
+        case (song, start) =>
+          (song.videoId, (getTime - start).toInt, song.originTitle)
       } pipeTo sender
     }
-    case Refill =>
-      titleSeq = titleSeq ++ Random.shuffle(titlesResource.lines().toSeq.filterNot(titleSeq contains _))
   }
 
-  private def pickWithTime() = {
-    pick().map {
-      case song =>
-        //val content = <p class="light">{ song.title }</p>.toString
-        //broadcaster ! ToAll(Json.stringify(Json.obj("type" -> "chat", "content" -> content)))
-        //chatLogger ! ChatLog(content)
-        (song, currentSecond)
-    }
-  }
-
-  private def pick(): Future[Song] = {
-    val replaces = replacesResource.lines().map(_.split(">>>")).map(a => (a.head, a.last)).toMap
-    val originTitle = titleSeq.head
+  private def getNextAndFill() = {
+    val nextSong = MusicStore.updateAndGetSong(titleSeq.head).map(song => {
+      //song changed notification
+      /*val content = <p class="light">{ song.title }</p>.toString
+      broadcaster ! ToAll(Json.stringify(Json.obj("type" -> "chat", "content" -> content)))
+      chatLogger ! ChatLog(content)*/
+      (song, getTime)
+    })
     titleSeq = titleSeq.tail
-    if (titleSeq.length < 11) self ! Refill
-    for {
-      id <- replaces.get(originTitle) match {
-        case Some(id) => Future.successful(id)
-        case None     => getFutureId(originTitle)
-      }
-      (title, duration) <- getFutureDetails(id)
-    } yield Song(originTitle, id, title, duration)
+    if (titleSeq.length < 20) {
+      titleSeq = titleSeq :+ Random.shuffle(titlesResource.lines()).find(!titleSeq.contains(_)).get
+    }
+    nextSong
   }
 
-  private def getFutureId(title: String): Future[String] = {
-    val futureId = WS.url("https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q="
-      + helper.urlEncode(title)
-      + "&type=video&fields=items%2Fid&key="
-      + googleKey)
-      .get
-      .map(response => (response.json \\ "videoId").head.as[String])
-    futureId.onFailure {
-      case e: Exception => Resource.fromFile("problem-title").write(title + "\n")
-    }
-    futureId
-  }
-
-  private def getFutureDetails(id: String): Future[(String, Int)] = {
-    val futureDetails = WS.url("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id="
-      + id
-      + "&fields=items(contentDetails%2Csnippet)&key="
-      + googleKey)
-      .get
-      .map { response =>
-        val title = (response.json \\ "title").head.as[String]
-        val mAndS = (response.json \\ "duration").head.as[String].replaceAll("[PTS]", "").split("M")
-        val duration = mAndS.head.toInt * 60 + mAndS.last.toInt
-        (title, duration)
-      }
-    futureDetails.onFailure {
-      case e: Exception => Resource.fromFile("problem-id").write(id + "\n")
-    }
-    futureDetails
+  private def removeAndFill(title: String) = {
+    //TODO
   }
 }
 
 case object AskSong
-case object Refill
