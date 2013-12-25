6b0f33debc11193e29da48e1d65fb6d8ffdcf1fc
pishen <pishen02@gmail.com>
2013-12-25 22:36:49 +0800

    change to lazy and big random list
    Former-commit-id: 25f0d78abff2397f13229919f564bc70de05982f [formerly 3a16ba706faa1a926114b65f325d4e0637b336e5] [formerly 8b7967740f50f69266c219baf289cec5e42f1e0b [formerly 5109e61736a5804148ee31bdc722e549e70b18f2]]
    Former-commit-id: 26372a0c86f441113568e6aab0dce8d7f0c6f1ee [formerly bc6e0dd2557ff81f4013e3da0c3cc996a6737b18]
    Former-commit-id: 1b1ef75f29abc1abea255c82f47aad21bb1c10b3

--- app/controllers/Playlist.scala
+++ app/controllers/Playlist.scala
@@ -21,9 +21,8 @@ class Playlist extends Actor {
   private val broadcaster = context.actorSelection("../broadcaster")
   private val chatLogger = context.actorSelection("../chatLogger")
   private def getTime() = new Date().getTime() / 1000
-  private val lowerBound = 50
-  private var buffer = Random.shuffle(titles.lines().toSeq).take(lowerBound + 1)
-    .map(ot => (ot, MusicStore.getSong(ot)))
+  private val lowerBound = 20
+  private var buffer = Random.shuffle(titles.lines().toSeq).map(SongHolder(_))
   private var nextSong = getNextAndFill()
 
   def receive = {
@@ -42,14 +41,14 @@ class Playlist extends Actor {
       } pipeTo sender
     }
     case ListContent => {
-      Future.sequence(buffer.take(20).map(_._2.map(song => {
+      Future.sequence(buffer.take(20).map(_.songF.map(song => {
         <img src={ song.thumb } title={ song.title } width="100"/>.toString
-      }).recover { case _ => <img src="error"/>.toString })) pipeTo sender
+      }).recover { case _ => <img src="error" width="0"/>.toString })) pipeTo sender
     }
   }
 
   private def getNextAndFill() = {
-    val nextSong = buffer.head._2.map(song => {
+    val nextSong = buffer.head.songF.map(song => {
       //song changed notification
       /*val content = <p class="light">{ song.title }</p>.toString
       broadcaster ! ToAll(Json.stringify(Json.obj("type" -> "chat", "content" -> content)))
@@ -58,9 +57,9 @@ class Playlist extends Actor {
     })
     buffer = buffer.tail
     if (buffer.length < lowerBound) {
-      val bufferTitles = buffer.map(_._1)
-      val newTitles = Random.shuffle(titles.lines()).toSeq.filterNot(bufferTitles.contains(_)).take(50)
-      buffer = buffer ++ newTitles.map(ot => (ot, MusicStore.getSong(ot)))
+      val bufferTitles = buffer.map(_.originTitle)
+      val newTitles = Random.shuffle(titles.lines().toSeq.filterNot(bufferTitles.contains(_)))
+      buffer = buffer ++ newTitles.map(SongHolder(_))
     }
     broadcaster ! ToAll(Json.stringify(Json.obj("type" -> "updateList")))
     nextSong
@@ -68,6 +67,10 @@ class Playlist extends Actor {
 
   private def removeAndFill(title: String) = {
     //TODO
+  }
+
+  case class SongHolder(originTitle: String) {
+    lazy val songF = MusicStore.getSong(originTitle)
   }
 }
 
