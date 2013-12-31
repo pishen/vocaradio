8940b79184741aa666335f87df696bd01a7fc78f
pishen <pishen02@gmail.com>
2014-01-01 03:13:49 +0800

    add FB login/logout
    Former-commit-id: e166a4e4f45881772b0a50a90b484479c577e813 [formerly f1616b1c8d54ee09aad2831d5b6e572467cedab6] [formerly 672396f386ff8b3f72c60cb7eccaa2e446c997e4 [formerly 3b18e00c5dacb8953dd7447cab585c40cd048861]]
    Former-commit-id: 1da426cd3f814f1b753b33298eefea8822d1b301 [formerly e5cdc04a821c4be60289940ec61c2687d540ecec]
    Former-commit-id: 2f3cabe2446c2fb20bf727d67d0ed9732309bbf6

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,37 +1,38 @@
-var originTitle;
+var originTitle
 $(document).ready(function() {
 	// insert script for ytplayer
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	var tag = document.createElement('script')
+	tag.src = "https://www.youtube.com/iframe_api"
+	var firstScriptTag = document.getElementsByTagName('script')[0]
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-	originTitle = document.title;
+	originTitle = document.title
 
-	setupUserName();
-	setupNotify();
-	setupNewMsg();
-	updateWS();
-	setupAliveMsg();
+	setupUserName()
+	setupNotify()
+	setupNewMsg()
+	updateWS()
+	setupAliveMsg()
+	setupFB()
 
-});
+})
 
 // username
-var userName;
+var userName
 function setupUserName() {
-	userName = $("#username");
+	userName = $("#username")
 	if (typeof (Storage) !== "undefined" && localStorage.userName) {
-		userName.val(localStorage.userName);
+		userName.val(localStorage.userName)
 	}
-	userName.blur(function(){
-		localStorage.userName = userName.val();
-	});
-	$("#new-msg").focusin(function(){
-		if(!userName.val()){
-			location.hash = "#setting";
-			userName.select();
+	userName.blur(function() {
+		localStorage.userName = userName.val()
+	})
+	$("#new-msg").focusin(function() {
+		if (!userName.val()) {
+			location.hash = "#setting"
+			userName.select()
 		}
-	});
+	})
 }
 
 // notification
@@ -42,14 +43,14 @@ function setupNotify() {
 				var notify = new Notification("VocaRadio", {
 					body : "訊息通知已啟動",
 					icon : "assets/images/logo.png"
-				});
+				})
 				window.setTimeout(function() {
-					notify.close();
-				}, 5000);
-			});
-			$(".tip").remove();
+					notify.close()
+				}, 5000)
+			})
+			$(".tip").remove()
 		}
-	});
+	})
 }
 
 // new chat POST
@@ -59,108 +60,108 @@ function setupNewMsg() {
 			var chatLog = {
 				user : userName.val(),
 				msg : $(this).val()
-			};
+			}
 			$.ajax({
 				url : "chat",
 				type : "POST",
 				data : JSON.stringify(chatLog),
 				contentType : "application/json; charset=utf-8"
-			});
-			$(this).val("");
-			return false;
+			})
+			$(this).val("")
+			return false
 		} else if (e.keyCode == 13) {
-			return false;
+			return false
 		}
-		document.title = originTitle;
+		document.title = originTitle
 	}).click(function() {
-		document.title = originTitle;
-	});
+		document.title = originTitle
+	})
 }
 
 // playlist
 function updatePlaylist() {
 	$.getJSON("listContent", function(jsObj) {
-		var playlist = $("#playlist");
-		var imgs = playlist.children();
+		var playlist = $("#playlist")
+		var imgs = playlist.children()
 		if (imgs.length == 0) {
 			$.each(jsObj.imgs, function(i, imgStr) {
-				$(imgStr).appendTo(playlist);
-			});
+				$(imgStr).appendTo(playlist)
+			})
 		} else {
 			imgs.each(function(i) {
-				var newImg = $(jsObj.imgs[i]).hide();
+				var newImg = $(jsObj.imgs[i]).hide()
 				$(this).delay(i * 100).fadeOut(function() {
-					$(this).replaceWith(newImg);
-					newImg.fadeIn();
-				});
-			});
+					$(this).replaceWith(newImg)
+					newImg.fadeIn()
+				})
+			})
 		}
-	});
+	})
 }
 
 // websocket
-var ws;
-var retryTimes = 0;
+var ws
+var retryTimes = 0
 function updateWS() {
-	ws = new WebSocket("ws://" + window.location.host + "/ws");
+	ws = new WebSocket("ws://" + window.location.host + "/ws")
 	ws.onopen = function() {
-		retryTimes = 0;
+		retryTimes = 0
 		// client counter
-		ws.send("still alive");
+		ws.send("still alive")
 		// chatroom
 		$.getJSON("chat-history", function(jsObj) {
-			$("#chat-log").children(".old").remove();
-			$("#chat-log").prepend(jsObj.history);
-			$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"));
-		});
-		updatePlaylist();
-	};
+			$("#chat-log").children(".old").remove()
+			$("#chat-log").prepend(jsObj.history)
+			$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"))
+		})
+		updatePlaylist()
+	}
 	ws.onmessage = function(e) {
-		var json = JSON.parse(e.data);
+		var json = JSON.parse(e.data)
 		if (json.type == "clientCount") {
-			$("#client-count").html(json.content);
+			$("#client-count").html(json.content)
 		} else if (json.type == "chat") {
-			$("#chat-log").append(json.content);
-			$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"));
+			$("#chat-log").append(json.content)
+			$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"))
 		} else if (json.type == "updateList") {
-			updatePlaylist();
+			updatePlaylist()
 		} else if (json.type == "notify") {
 			if ($("#notify").prop("checked") && json.title != userName.val()) {
 				var notify = new Notification(json.title, {
 					body : json.body,
 					icon : "assets/images/logo.png",
 					tag : json.tag
-				});
+				})
 				window.setTimeout(function() {
-					notify.close();
-				}, 5000);
-				document.title = "*" + originTitle;
+					notify.close()
+				}, 5000)
+				document.title = "*" + originTitle
 			}
 		}
-	};
+	}
 	ws.onclose = function() {
 		// chatroom
-		$("#chat-log").append("<p>(connection lost，reconnect in 2 sec)</p>");
-		$("#chat-log").children().addClass("old");
-		$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"));
+		$("#chat-log").append("<p>(connection lost，reconnect in 2 sec)</p>")
+		$("#chat-log").children().addClass("old")
+		$("#chat-log").scrollTop($("#chat-log").prop("scrollHeight"))
 		if (retryTimes < 10) {
-			window.setTimeout(updateWS, 2000);
-			retryTimes += 1;
+			window.setTimeout(updateWS, 2000)
+			retryTimes += 1
 		}
-	};
+	}
 }
 
 // keep websocket connected by nginx
 function setupAliveMsg() {
 	window.setInterval(function() {
-		ws.send("still alive");
-	}, 60000);
+		ws.send("still alive")
+	}, 60000)
 }
 
 // YouTube player
-var isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-var player;
+var isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
+var player
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height : '360',
@@ -177,57 +178,88 @@ function onYouTubeIframeAPIReady() {
 			'onStateChange' : onPlayerStateChange,
 			'onError' : onPlayerError
 		}
-	});
+	})
 }
 
 function onPlayerReady(event) {
-	console.log("ready");
-	player.setVolume($("#volume").prop("value"));
+	console.log("ready")
+	player.setVolume($("#volume").prop("value"))
 	$("#volume").change(function() {
-		player.setVolume($(this).prop("value"));
-	});
+		player.setVolume($(this).prop("value"))
+	})
 	if (isFirst)
-		player.loadVideoById('7NptssoOJ78');
+		player.loadVideoById('7NptssoOJ78')
 	else
-		syncAndPlay(true);
+		syncAndPlay(true)
 }
 
-var prePlayerState;
-var isFirst = true;
+var prePlayerState
+var isFirst = true
 function onPlayerStateChange(event) {
 	if (event.data == YT.PlayerState.ENDED) {
-		console.log("ended");
-		syncAndPlay(isFirst);
-		isFirst = false;
+		console.log("ended")
+		syncAndPlay(isFirst)
+		isFirst = false
 	} else if (event.data == YT.PlayerState.PLAYING && !isMobile) {
-		console.log("playing");
+		console.log("playing")
 		if (prePlayerState == YT.PlayerState.PAUSED
 				|| prePlayerState == YT.PlayerState.ENDED) {
-			syncAndPlay(true);
+			syncAndPlay(true)
 		}
 		$("#playback").text("Pause").off().click(function() {
-			player.pauseVideo();
-		});
+			player.pauseVideo()
+		})
 	} else if (event.data == YT.PlayerState.PAUSED && !isMobile) {
 		$("#playback").text("Play").off().click(function() {
-			syncAndPlay(true);
-		});
+			syncAndPlay(true)
+		})
 	}
-	prePlayerState = event.data;
+	prePlayerState = event.data
 }
 
 function onPlayerError(event) {
-	console.log("player error: " + event.data);
+	console.log("player error: " + event.data)
 	$("#playback").text("Play").off().click(function() {
-		syncAndPlay(true);
-	});
+		syncAndPlay(true)
+	})
 }
 
 function syncAndPlay(seek) {
-	// console.log("seek: " + seek);
+	// console.log("seek: " + seek)
 	$.getJSON("sync", function(jsObj) {
-		console.log("sync id: " + jsObj.id);
-		player.loadVideoById(jsObj.id, seek ? jsObj.start : 0);
-		player.setVolume($("#volume").prop("value"));
-	});
+		console.log("sync id: " + jsObj.id)
+		player.loadVideoById(jsObj.id, seek ? jsObj.start : 0)
+		player.setVolume($("#volume").prop("value"))
+	})
+}
+
+// Facebook
+function setupFB() {
+	$.getScript('//connect.facebook.net/en_US/all.js', function() {
+		FB.init({
+			appId : '565192206888536',
+			status : true,
+			cookie : true,
+			xfbml  : false
+		})
+		$("#login").click(function(){
+			FB.login()
+		})
+		$("#logout").click(function(){
+			FB.logout()
+		}).hide()
+		FB.Event.subscribe('auth.authResponseChange', function(resp) {
+			if (resp.status === 'connected') {
+				$("#login").hide()
+				$("#logout").show()
+				FB.api('/me', function(resp) {
+					$("#fb-status").text(" (Logged in as " + resp.name + ") ")
+				})
+			}else{
+				$("#login").show()
+				$("#logout").hide()
+				$("#fb-status").text("")
+			}
+		})
+	})
 }
