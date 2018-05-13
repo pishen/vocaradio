3bc24fa068270a16db703ce284b25aa12f8b0105
Pishen Tsai <pishen02@gmail.com>
2018-05-13 17:00:00 +0800

    store user name in localStorage
    Former-commit-id: f46be84718c897b13da7e7f40688f03fa3ec38a0 [formerly e4783fe51deb13c1ff118d7489949c328ba69e69] [formerly 816fadf0eb00cf2e537a61a778b95193d3c49fc8 [formerly 57dfcb9e8c910349f00e7f7d1607f1a64942e5e8]] [formerly 235d864550d69a1990670d8cac98abc2befbaecc [formerly 2ee39b94fecb83880823fafaeea9019e11a54f82] [formerly 4e800da67c126d42926f54685d91d81cfe309741 [formerly 4f2e9ede05a3d0ce9158ef7bd48734236ba48272]]]
    Former-commit-id: b501f55e12b32e40b6b61b1224eb907a5ab59fe9 [formerly 1d299e7871b079fea838c137ff1fcfae11c8bf9a] [formerly 6cd2f1fd0abed46650623cee9fc553540b64ce8d [formerly b2bfe378c096aa1b1684b6fc2667d44d0931c93c]]
    Former-commit-id: d90b0657855fa6522ef42615a60c3298c38eea4d [formerly d8b42a3c3d68e29e47652922bfe0381cd70fae44]
    Former-commit-id: ace8d315c9a721da646749b0e57045153d3a2780

--- frontend/src/main/scala/vocaradio/Main.scala
+++ frontend/src/main/scala/vocaradio/Main.scala
@@ -50,12 +50,37 @@ object Main {
     println("Welcome to VocaRadio!")
 
     // portal
-    val portalNameInput = input(CSS.textarea).render
+    val portalNameInput = input(
+      CSS.textarea,
+      value := Option(window.localStorage.getItem("username")).getOrElse("")
+    ).render
+
+    def getUserName(): Option[String] = {
+      Some(portalNameInput.value).filter(_ != "")
+    }
+
+    val portalNameTooltip = div(
+      CSS.tooltip,
+      div(CSS.tooltipArrow),
+      "請輸入暱稱"
+    ).render
+
+    portalNameInput.onchange = { (e: Event) =>
+      getUserName() match {
+        case Some(name) =>
+          window.localStorage.setItem("username", name)
+          portalNameTooltip.hide()
+        case None =>
+          window.localStorage.removeItem("username")
+          portalNameTooltip.show()
+      }
+    }
+    portalNameInput.asInstanceOf[js.Dynamic].onchange()
 
     val portalNameDiv = div(
       CSS.portalName,
       portalNameInput,
-      div(CSS.tooltip, div(CSS.tooltipArrow), "請輸入暱稱")
+      portalNameTooltip
     ).render
     portalNameDiv.hide()
 
